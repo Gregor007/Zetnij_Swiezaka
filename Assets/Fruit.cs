5ce43d04c654d71216de69effaf81300cd766f9f
Grzegorz Janusz <grzegorz.janusz.7.1995@gmail.com>
2017-11-25 21:26:53 +0100

    TO DO: zmiana animacji przekrojonego arbuza (obrócić)

--- Assets/Fruit.cs
+++ Assets/Fruit.cs
@@ -6,6 +6,15 @@ public class Fruit : MonoBehaviour
 {
 
     public GameObject fruitSlicedPrefab;
+    public float startForce = 15f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
